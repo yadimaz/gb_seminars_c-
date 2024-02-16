6379f54d1eeb606c2ecd4f0bb68049fe9136df66
Dmitry Zakharov <ya.dimaz@gmail.com>
2024-02-16 11:21:35 +0300

    Exec DZ1.3

---
+++ DZ1.3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Внутри класса Answer напишите метод CheckIfEven, который на вход принимает число number
+// и выводит, является ли число чётным (делится ли оно на два без остатка).
+
+using System;
+
+class Answer {
+    static void CheckIfEven(int number)
+    {
+      // Введите свое решение ниже
+        if (number %2 == 0)
+        {
+            Console.WriteLine("четное");
+        }
+        if (number % 2 == 1)
+        {
+            Console.WriteLine("нечетное");
+        }
+    }
+  
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 8;
+        }
+
+        // Не удаляйте строки ниже
+        CheckIfEven(number);
+    }
+}
