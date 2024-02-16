2efaf252f461b7b6c125514c53dfa2621a1eb932
Dmitry Zakharov <ya.dimaz@gmail.com>
2024-02-16 11:36:30 +0300

    Exec DZ1.4

---
+++ DZ1.4/Program.cs
@@ -0,0 +1,33 @@
+﻿// Внутри класса Answer напишите метод PrintEvenNumbers, которая на вход принимает число (number), 
+//а на выходе выводит все чётные числа от 1 до number (включительно), после каждого числа должен быть знак пробела.
+
+using System;
+
+public class Answer {
+    static void PrintEvenNumbers(int number)
+    {
+      // Введите свое решение ниже
+    int num = 2;
+    while (num <= number)
+        {
+            Console.Write($"{num} ");
+            num+=2;
+        }
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = -15;
+        }
+
+        // Не удаляйте строки ниже
+        PrintEvenNumbers(number);
+    }
+}
