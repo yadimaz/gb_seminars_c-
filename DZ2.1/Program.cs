db69b9286f415f0e0834793b66f507b062b54733
Захаров Дмитрий Сергеевич <dzakharov@office.er76.ru>
2024-02-21 12:43:35 +0300

    Execute DZ 2.1

--- DZ2.1/Program.cs
+++ DZ2.1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+
+Console.WriteLine("Vvedite chislo");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num%7==0 && num%23==0) Console.WriteLine($"Chislo {num} krato 7 and 23");
+else Console.WriteLine($"Chislo {num} ne krato 7 and 23");
