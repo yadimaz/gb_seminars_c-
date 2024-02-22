3444a0923a952a56a233641706a323cf6230ce12
Dmitry Zakharov <ya.dimaz@gmail.com>
2024-02-22 17:05:58 +0300

    Execute DZ2.4

--- DZ2.4/Program.cs
+++ DZ2.4/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+string num = Convert.ToString(new Random().Next());
+
+
+for (int i=0; i<num.Length;i++)
+{
+    Console.Write($"{num[i]}, ");
+}
+
