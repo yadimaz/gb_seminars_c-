f0832265e5fb9ea7c1f8ac96a595f5a6c4cd2ad8
Dmitry Zakharov <ya.dimaz@gmail.com>
2024-02-23 11:18:47 +0300

    Corrected DZ2.4

--- DZ2.4/Program.cs
+++ DZ2.4/Program.cs
@@ -1,37 +1,20 @@
-﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
-//string num = Convert.ToString(new Random().Next());
-string num = "1234";
-int kef = num.Length;
-int num_int = Convert.ToInt32(num);
-Console.WriteLine(num_int);
-Console.WriteLine(kef);
+﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N, 
+//а на выходе показывает его цифры через запятую.
+int num = new Random().Next();
+Console.WriteLine($"{num} - Original Random number");
 
-int nnu = 0;
+int m = num;
+int count = 0;
 
-while (kef >= 0)
-{
-    // Console.Write(num_int % Math.Pow(10, kef));
-    //Console.WriteLine(kef);
-  
-    nnu = Convert.ToInt32(Math.Pow(10, kef - 1));
-    //Console.WriteLine(nnu);
-    Console.WriteLine((num_int / Convert.ToInt32(Math.Pow(10, kef) ) % 10));
-    //Console.Write($"{num[i]}, ");
-    kef = kef - 1;
-}
-
-Console.Write("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int m = n;
-int conunt = 0;
 while (m > 0)
 {
-  conunt++;
+  count++;
   m /= 10;
 }
 
-while (conunt > 0)
+while (count > 0)
 {
-  Console.Write($"{n / Convert.ToInt32(Math.Pow(10, conunt - 1)) % 10}, ");
-  conunt--;
+  Console.Write($"{num / Convert.ToInt32(Math.Pow(10, count - 1)) % 10}");
+  if (count > 1) Console.Write($", ");
+  count--;
 }
