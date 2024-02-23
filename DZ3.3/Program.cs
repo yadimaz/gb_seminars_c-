2deb3d164321f8a6676c1e61e70ff1a4c85a04de
Dmitry Zakharov <ya.dimaz@gmail.com>
2024-02-23 16:18:18 +0300

    Execute DZ3.3

--- DZ3.3/Program.cs
+++ DZ3.3/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив из вещественных чисел с ненулевой дробной частью. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// Начальные условия:
+// int[] array = {0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01}; // Создание массива
+// Выводится: 5.15
+
+using System;
+using System.Linq;
+
+//Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+class UserInputToCompileForTest
+{ 
+    // Разница между максимальным и минимальным элементами массива
+    
+    // Нахождение минимума массива
+    public static double FindMin(double[] numbers)
+    {
+        double min_cnt = numbers[0];
+        foreach (var item in numbers)
+        {
+            if(item < min_cnt) min_cnt = item;
+        }
+        return min_cnt;
+    }
+    
+    // Нахождение максимума массива
+    public static double FindMax(double[] numbers)
+    {
+        double max_cnt = numbers[0];
+        foreach (var item in numbers)
+        {
+            if(item > max_cnt) max_cnt = item;
+        }
+        return max_cnt;
+    }
+    
+        
+    public static void PrintResult(double[] array)
+    {
+        double min_cnt = FindMin(array);
+        double max_cnt = FindMax(array);
+        Console.Write(max_cnt - min_cnt);
+    }
+}
+
+//Не удаляйте и не меняйте класс Answer!
+class Answer
+{
+    public static void Main(string[] args)
+    {
+        double[] array;
+        
+
+        if (args.Length >= 1) {
+            // Объединяем все аргументы командной строки в одну строку
+            string joinedArgs = string.Join(" ", args);
+
+            // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
+            array = joinedArgs.Split(", ")
+                                  .Select(double.Parse)
+                                  .ToArray();
+            
+            // Теперь arr содержит преобразованные в целые числа из командной строки
+        
+        } else {
+           // Если аргументов на входе нет
+            array = new double[] {0.0, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01}; // Создание массива
+        }
+        UserInputToCompileForTest.PrintResult(array);
+    }
+}
