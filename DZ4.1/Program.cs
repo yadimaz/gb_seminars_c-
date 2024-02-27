db5b0e711a1c160e1f878505ebceeebd1cb6e29b
Dmitry Zakharov <ya.dimaz@gmail.com>
2024-02-27 11:44:09 +0300

    Execute DZ4.1

--- DZ4.1/Program.cs
+++ DZ4.1/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+//Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого четная.
+
+using System.Dynamic;
+using System.Globalization;
+
+Console.Clear();
+
+int SumNum(int num)
+{
+    int sumnum = 0;
+    if (num / 10 == 0) return num;
+    else
+    {
+        while (num != 0)
+        {
+            sumnum += (num % 10);
+            num /= 10;
+        }
+        return sumnum;
+    }
+}
+
+bool CheckEven(int num)
+{
+    return (num % 2 == 0);
+}
+
+while (true)
+{
+    Console.Write("Введите целое число:");
+    string vvod = Console.ReadLine()!;
+    if (int.TryParse(vvod, out int number))
+    {
+        number = SumNum(number);
+        if(CheckEven(number)== true) break;
+    }
+    else if (vvod.Length == 1 && vvod[0] == 'q') break;
+}
