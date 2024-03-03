8afe122cfe0a9c86c8d22472ecb3a2b17ac25bcd
Dmitry Zakharov <ya.dimaz@gmail.com>
2024-03-03 16:08:29 +0300

    Execute DZ5.3

--- DZ5.3/Program.cs
+++ DZ5.3/Program.cs
@@ -1,2 +1,90 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+using System;
+using Microsoft.VisualBasic;
+
+//Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+class UserInputToCompileForTest
+{
+    /// Вычисление сумм по строкам (на выходе массив с суммами строк)
+    public static int[] SumRows(int[,] array)
+    {
+        int[] itemsum = new int[array.GetLength(0)];
+        int sumitem = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sumitem = array[i, 0];
+            for (int j = 1; j < array.GetLength(1); j++)
+            {
+                sumitem += array[i, j];
+            }
+            itemsum[i] = sumitem;
+        }
+        return itemsum;
+    }
+
+    // Получение индекса минимального элемента в одномерном массиве
+    public static int MinIndex(int[] array)
+    {
+        int min = 0;
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] < array[min]) min = i;
+        }
+        return min;
+    }
+    public static void PrintResult(int[,] numbers)
+    {
+        Console.WriteLine(MinIndex(SumRows(numbers)));
+    }
+}
+
+//Не удаляйте и не меняйте класс Answer!
+class Answer
+{
+    public static void Main(string[] args)
+    {
+        int[,] numbers;
+
+        if (args.Length >= 1)
+        {
+            // Предполагается, что строки разделены запятой и пробелом, а элементы внутри строк разделены пробелом
+            string[] rows = args[0].Split(',');
+
+            int rowCount = rows.Length;
+            int colCount = rows[0].Trim().Split(' ').Length;
+
+            numbers = new int[rowCount, colCount];
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                string[] rowElements = rows[i].Trim().Split(' ');
+
+                for (int j = 0; j < colCount; j++)
+                {
+                    if (int.TryParse(rowElements[j], out int result))
+                    {
+                        numbers[i, j] = result;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error parsing element {rowElements[j]} to an integer.");
+                        return;
+                    }
+                }
+            }
+        }
+        else
+        {
+            // Если аргументов на входе нет, используем примерный массив
+
+            numbers = new int[,] {
+                {1, 2, 3},
+                {1, 1, 0},
+                {7, 8, 2},
+                {9, 10, 11}
+    };
+        }
+        UserInputToCompileForTest.PrintResult(numbers);
+    }
+}
