c4f2cf0bd52749d0b2a9bcde792931a9575e5f52
Dmitry Zakharov <ya.dimaz@gmail.com>
2024-03-05 17:37:48 +0300

    Execute DZ6.4

--- DZ6.4/Program.cs
+++ DZ6.4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4*(не обязательная): Задайте строку, состоящую из слов, разделенных пробелами. 
+// Сформировать строку, в которой слова расположены в обратном порядке. 
+// В полученной строке слова должны быть также разделены пробелами.
+
+string phrase = "The quick brown fox jumps over the lazy dog.";
+string[] words = phrase.Split(' ');
+
+for (int i = 0; i < words.Length; i++)
+{
+    Console.Write($" {words[words.Length - 1 - i]}");
+}
