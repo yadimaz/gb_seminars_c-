f0440319d9d48baf6a5eafede898fa949768d9a4
Dmitry Zakharov <ya.dimaz@gmail.com>
2024-03-09 21:32:03 +0300

    Execute DZ7.3

--- DZ7.3/Program.cs
+++ DZ7.3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы
+
+Console.Clear();
+int[] mas = {1,2,5,0,10,34};
+
+void PrintArrayRecurse(int len)
+{
+    Console.Write($"{mas[len]} ");
+    if(len > 0) PrintArrayRecurse(len - 1);
+}
+
+int len = mas.Length - 1;
+
+Console.Write($"[{string.Join(", ", mas)}] => ");
+PrintArrayRecurse(len);
+
